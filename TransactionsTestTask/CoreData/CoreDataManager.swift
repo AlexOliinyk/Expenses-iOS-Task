c64c6fae124aaaf697fbb236dfac79544325faf2
Alex Oliinyk <alex.oliinyk@gmail.com>
2025-01-19 19:44:20 +0100

    add comments and small improvements to CoreDataManager

--- TransactionsTestTask/CoreData/CoreDataManager.swift
+++ TransactionsTestTask/CoreData/CoreDataManager.swift
@@ -10,30 +10,52 @@ import CoreData
 class CoreDataManager {
     static let shared = CoreDataManager()
     private init() {}
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
+        // Initialize a Core Data persistent container with the name of the data model
         let container = NSPersistentContainer(name: "TransactionsTestTask")
+        // Load the persistent stores
         container.loadPersistentStores { _, error in
             if let error = error as NSError? {
+                // Handle any errors during loading, terminating the app if necessary
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         }
         return container
     }()
-
+    
     var context: NSManagedObjectContext {
         return persistentContainer.viewContext
     }
-
+    
+    // for saving context
     func saveContext() {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
+        do {
+            try context.save()
+        } catch {
+            print("Core Data save error: \(error)")
         }
+    }
+    
+    // to get the balance in wallet
+    func fetchWallet() -> Wallet? {
+        let request: NSFetchRequest<Wallet> = Wallet.fetchRequest()
+        do {
+            let wallets = try context.fetch(request)
+            return wallets.first
+        } catch {
+            print("Error fetching wallet: \(error)")
+            return nil
+        }
+    }
+    
+    // Create a new wallet
+    func createWallet() -> Wallet {
+        let wallet = Wallet(context: context)
+        wallet.balance = 0.0
+        wallet.cachedExchangeRate = 0.0
+        wallet.lastUpdated = Date()
+        saveContext()
+        return wallet
     }
 }
